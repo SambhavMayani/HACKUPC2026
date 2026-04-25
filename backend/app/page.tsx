962b4c5623f3871cb858df5239dc750f9c575cfb
Pere Gomila <pegoku08@gmail.com>
2026-04-25 20:35:46 +0200

    cleanup

--- backend/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { Dashboard } from "@/components/dashboard";
-import { getDashboardData } from "@/lib/creative-intelligence";
-
-export default async function Page() {
-  const data = await getDashboardData();
-  return <Dashboard data={data} />;
-}
